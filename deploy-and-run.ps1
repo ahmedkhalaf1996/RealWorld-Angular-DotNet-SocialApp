a2dabfaa3443d5e132314ae02eaa268bb308db5a
Ahmed Khalaf <khalafahmed1996@gmail.com>
2026-04-04 09:01:16 +0200

    first auto deploy

---
+++ deploy-and-run.ps1
@@ -0,0 +1,40 @@
+# Deploy And Run Script 
+Write-Host "========================================" -ForegroundColor Cyan
+Write-Host "Deploying and Running the Application..." -ForegroundColor Green
+Write-Host "========================================" -ForegroundColor Cyan
+
+# 1 Ask For Version 
+$version = Read-Host "Enter the version to deploy (e.g., v1.0.0)"
+if ([string]::IsNullOrWhiteSpace($version)) {
+    Write-Host "Version cannot be empty. Exiting." -ForegroundColor Red
+    exit 1
+}
+
+# 2 Ask For Commit Mesage
+$commitMessage = Read-Host "Enter the commit message "
+if ([string]::IsNullOrWhiteSpace($commitMessage)) {
+    $commitMessage = "Release  $version"
+}
+
+# 3 Todo: Update kubernetes MainFests
+
+# 4 Push To gitHub
+Write-Host "n[2/4] Pushing changes and tags to GitHub..." -ForegroundColor Yellow
+git add .
+git commit -m "$commitMessage"
+git tag $version
+git push -u origin HEAD --tags
+
+Write-Host "Changes and tags pushed to GitHub successfully." -ForegroundColor Green
+write-Host "GitHub Actions Is now building. docker iamges for version '$version'." -ForegroundColor Cyan
+write-Host "Please wait for 'Buikd and Push Docker images ' workflow to complete." -ForegroundColor Cyan
+
+# 5 Wait For onfirmation
+$confirmation = Read-Host "`n Is The Github Actions Finished? (y/n)"
+if ($confirmation -ne 'y') {
+    Write-Host "Please wait for the GitHub Actions to finish before running the application." -ForegroundColor Red
+    exit 1
+}
+
+write-Host "GitHub Actions completed. Proceeding to deploy to Kubernetes..." -ForegroundColor Green
+# 6 Todo Deploy To Kubernetes 
