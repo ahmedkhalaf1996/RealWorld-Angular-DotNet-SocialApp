b90fa00e6a54dda0182b7a47be9ba36345e8e696
Ahmed Khalaf <khalafahmed1996@gmail.com>
2026-04-12 02:44:04 +0200

    new updated v1

---
+++ clean-cluster.ps1
@@ -0,0 +1,23 @@
+# clean cluster script 
+Write-host "Cleaning cluster..."
+
+$confirmation = Read-Host "Are you sure you want to clean the cluster? This action cannot be undone. (y/n)"
+if ($confirmation -ne "y") {
+    Write-Host "Cluster Cancelled" -foregroundcolor Yellow
+    exit 0
+}
+
+write-host "n Deleting cluster resources..." -foregroundcolor Yellow
+
+kubectl delete -f k8s/ingress.yaml --ignore-not-found
+kubectl delete -f k8s/ingress-chat.yaml --ignore-not-found
+kubectl delete -f k8s/ingress-notification.yaml --ignore-not-found
+kubectl delete -f k8s/api.yaml --ignore-not-found
+kubectl delete -f k8s/chat.yaml --ignore-not-found
+kubectl delete -f k8s/notification.yaml --ignore-not-found
+kubectl delete -f k8s/frontend.yaml --ignore-not-found
+kubectl delete -f k8s/mongo.yaml --ignore-not-found
+
+write-host "Cluster cleaned successfully." -foregroundcolor Green
+
+
