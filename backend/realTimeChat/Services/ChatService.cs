1c3e415df95eb28a9942c6f29fe26b5184450701
Ahmed Khalaf <khalafahmed1996@gmail.com>
2026-04-16 16:05:50 +0200

    redis debug

--- backend/realTimeChat/Services/ChatService.cs
+++ backend/realTimeChat/Services/ChatService.cs
@@ -1,9 +1,8 @@
-
+using realTimeServices.Dtos;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
 using StackExchange.Redis;
-using realTimeServices.Dtos;
+using System.Text.Json;
 
 namespace realTimeServices.Services
 {
@@ -12,13 +11,11 @@ namespace realTimeServices.Services
         private readonly IDatabase _db;
         private readonly RealTimeChatClient _client;
 
-        // IConnectionMultiplexer is injected natively so it connects to your k8s Redis container
         public ChatService(IConnectionMultiplexer redis, RealTimeChatClient client)
         {
             _db = redis.GetDatabase();
             _client = client;
         }
-
         public void SaveMessageToDb(MessageDto message)
         {
             _client.SendMessage(message.content, message.sender, message.recever);
@@ -26,58 +23,52 @@ namespace realTimeServices.Services
 
         public List<string> AddAndGetUserRooms(string userid)
         {
+            var client = new RealTimeChatClient();
             List<string> uides = new List<string>();
 
-            if (userid != "undefined" && !string.IsNullOrEmpty(userid))
+            if (userid != "undefined" && userid != "" && userid is not null)
             {
                 var uidesx = _client.GetUsersIdes(userid);
-
-                foreach (var id in uidesx)
+                foreach( var id in uidesx)
                 {
-                    // Redis: Check if friend's room exists globally
-                    if (_db.HashExists("UserRooms", id))
+                   if(_db.HashExists("UserRooms", id))
+                   {
+                    if (!uides.Contains(id))
                     {
-                        if (!uides.Contains(id)) uides.Add(id);
+                        uides.Add(id);
                     }
+                   }
                 }
 
-                // Update this user's friends list globally in Redis
                 _db.HashSet("UserRooms", userid, JsonSerializer.Serialize(uides));
 
                 foreach (var uid in uides)
                 {
-                    // Pull friend's room from Redis
-                    var otherRoomJson = _db.HashGet("UserRooms", uid);
-                    List<string> otherRoom = otherRoomJson.HasValue
-                        ? JsonSerializer.Deserialize<List<string>>(otherRoomJson.ToString()) ?? new List<string>()
-                        : new List<string>();
-
-                    if (!otherRoom.Contains(userid))
-                    {
-                        otherRoom.Add(userid);
-                        // Save friend's updated room back to Redis
-                        _db.HashSet("UserRooms", uid, JsonSerializer.Serialize(otherRoom));
-                    }
+                   var otherRoomJson = _db.HashGet("UserRooms", uid);
+                   List<string> otherRoom = otherRoomJson.HasValue ? 
+                   JsonSerializer.Deserialize<List<string>>(otherRoomJson.ToString()) ?? new List<string>()
+                   : new List<string>();
+                     if (!otherRoom.Contains(userid))
+                     {
+                      otherRoom.Add(userid);
+                      _db.HashSet("UserRooms", uid, JsonSerializer.Serialize(otherRoom));
+                     }
                 }
-
                 uides.Add(userid);
             }
-
             return uides;
         }
 
-        public List<string> RemoveUserFromList(string userid)
+        public List<string>? RemoveUserFromList(string userid)
         {
-            if (userid != "undefined" && !string.IsNullOrEmpty(userid))
+            if (userid != "undefined" && userid != "" && userid is not null)
             {
-                // Globally remove connection ID 
-                _db.HashDelete("OnlineUsers", userid);
-
+                _db.HashDelete("UserRooms", userid);
                 var roomsJson = _db.HashGet("UserRooms", userid);
                 if (!roomsJson.HasValue) return null;
 
                 var uides = JsonSerializer.Deserialize<List<string>>(roomsJson.ToString()) ?? new List<string>();
-                var removedlist = uides.ToList();
+                var removedList = uides.ToList();
 
                 foreach (var uid in uides)
                 {
@@ -89,279 +80,81 @@ namespace realTimeServices.Services
                         {
                             _db.HashSet("UserRooms", uid, JsonSerializer.Serialize(otherRoom));
                         }
-                    }
+                    } 
                 }
-
                 // Globally remove user room
                 _db.HashDelete("UserRooms", userid);
-                removedlist.Add(userid);
-                return removedlist;
+                removedList.Add(userid);
+                return removedList;
+
             }
             return null;
         }
 
-        public string[] GetOnlineUsers(string id)
+        public string[]? GetOnlineUsers(string id)
         {
             try
             {
                 var online = GetOnlyUserRooms(id);
-                return online?.ToArray() ?? new string[] { };
+                return online?.ToArray() ?? new string[] {};
             }
-            catch
+            catch 
             {
-                return null;
+                
+               return null;
             }
         }
 
-        public List<string> GetOnlyUserRooms(string userid)
+        public List<string>? GetOnlyUserRooms(string userid)
         {
             try
             {
-                // Read from global Redis cache instead of local Dictionary
                 var roomsJson = _db.HashGet("UserRooms", userid);
                 if (roomsJson.HasValue)
                 {
                     return JsonSerializer.Deserialize<List<string>>(roomsJson.ToString()) ?? new List<string>();
                 }
                 return null;
+
             }
-            catch
+            catch 
             {
-                return null;
+                
+              return null;
             }
         }
 
-        // connection method
         public void AddUserConnectionId(string userid, string connectionId)
         {
-            if (userid != "undefined" && !string.IsNullOrEmpty(userid) &&
-                connectionId != "undefined" && !string.IsNullOrEmpty(connectionId))
+            if (userid != "undefined" && !string.IsNullOrEmpty(userid) && connectionId != "undefined" && !string.IsNullOrEmpty(connectionId))
             {
-                // Store connection ID globally in Redis Hash
-                _db.HashSet("OnlineUsers", userid, connectionId);
+              _db.HashSet("OnlineUsers", userid, connectionId);
             }
         }
 
+        // get user by connction id 
         public string GetUserIdByConnectionID(string connectionId)
         {
             if (connectionId != "undefined" && !string.IsNullOrEmpty(connectionId))
             {
-                // Retrieve all online users from Redis to find matching connectionId
                 var onlineUsers = _db.HashGetAll("OnlineUsers");
                 var user = onlineUsers.FirstOrDefault(x => x.Value == connectionId);
-                return user.Key.HasValue ? user.Key.ToString() : "";
+                return user.Name.HasValue ? user.Name.ToString() : "";
             }
             return "";
         }
 
-        public string GetConnectionIdByUser(string user)
+        public string GetConnectionIdByUser(string user) // user represent userid
         {
             if (user != "undefined" && !string.IsNullOrEmpty(user))
             {
-                // Fetch connection ID directly from Redis Hash
-                var connId = _db.HashGet("OnlineUsers", user);
-                return connId.HasValue ? connId.ToString() : "";
+                var connectionId = _db.HashGet("OnlineUsers", user);
+                return connectionId.HasValue ? connectionId.ToString() : "";
             }
             return "";
         }
+
+
+
     }
 }
-
-
-// using realTimeServices.Dtos;
-// using System.Collections.Generic;
-// using System.Linq;
-
-
-// namespace realTimeServices.Services
-// {
-//     public class ChatService
-//     {
-//         private static readonly Dictionary<string, string> Users = new Dictionary<string, string>();
-
-//         public class Rooms
-//         {
-//             public Dictionary<string , List<String>> Room {get; set;} = new Dictionary<string, List<string>>();
-//         }
-
-//         Rooms room = new Rooms();
-
-//         public void SaveMessageToDb(MessageDto message)
-//         {
-//             var client = new RealTimeChatClient();
-//             client.SendMessage(message.content, message.sender, message.recever);
-//         }
-
-//         public List<string> AddAndGetUserRooms(string userid)
-//         {
-//             var client = new RealTimeChatClient();
-//             List<string> uides = new List<string>();
-
-//             if (userid != "undefined" && userid != "" && userid is not null)
-//             {
-//                 var uidesx = client.GetUsersIdes(userid);
-//                 uidesx.ForEach((id) =>
-//                 {
-//                   if (room.Room.ContainsKey(id))
-//                     {
-//                         //uides.Add(id);
-//                         if (!uides.Contains(id)) uides.Add(id);
-//                     }  
-//                 });
-
-//                 if (room.Room.ContainsKey(userid))
-//                 {
-//                     room.Room[userid] = uides;
-//                 } else
-//                 {
-//                     room.Room.Add(userid, uides);
-//                 }
-
-//                 // add userid to other users rooms
-//                 foreach(var uid in uides)
-//                 {
-//                     if (room.Room.ContainsKey(uid))
-//                     {
-//                         // room.Room[uid].Add(userid);
-//                         if (!room.Room[uid].Contains(userid)) room.Room[uid].Add(userid);
-//                     }
-//                 }
-
-//                 uides.Add(userid);
-//             }
-//             return uides;
-//         }
-
-//         public List<string>? RemoveUserFromList(string userid)
-//         {
-//             if (userid != "undefined" && userid != "" && userid is not null)
-//             {
-//                 lock (Users)
-//                 {
-//                     if (Users.ContainsKey(userid))
-//                     {
-//                         Users.Remove(userid);
-//                     }
-//                 }
-
-//                 var uides = room.Room[userid];
-//                 var removedlist = room.Room[userid];
-
-//                 foreach (var uid in uides.ToArray())
-//                 {
-//                     if (room.Room.ContainsKey(uid))
-//                     {
-//                         room.Room[uid].Remove(userid);
-//                     }
-//                 }
-
-//                 if (room.Room.ContainsKey(userid))
-//                 {
-//                     room.Room.Remove(userid);
-//                 }
-
-//                 removedlist.Add(userid);
-//                 return removedlist;
-//             } else
-//             {
-//                 return null;
-//             }
-
-//         }
-
-//         public string[]? GetOnlineUsers(string id)
-//         {
-//             try
-//             {
-//                 var online = room.Room[id].ToArray();
-//                 return online;
-//             }
-//             catch 
-//             {
-                
-//                return null;
-//             }
-//         }
-
-//         public List<string>? GetOnlyUserRooms(string userid)
-//         {
-//             try
-//             {
-//                 List<string> uidesList = room.Room[userid];
-//                 return uidesList;
-//             }
-//             catch 
-//             {
-                
-//               return null;
-//             }
-//         }
-
-//         public void AddUserConnectionId(string userid, string connectionId)
-//         {
-//             if (
-//                 userid != "undefined" &&
-//                 userid != "" && 
-//                 userid is not null &&
-//                 connectionId != "undefined" &&
-//                 connectionId != "" && 
-//                 connectionId is not null 
-//                 )
-//             {
-//                 lock(Users)
-//                 {
-//                     if (!Users.ContainsKey(userid))
-//                     {
-//                         Users[userid] = connectionId;
-//                     }
-//                 }
-//             }
-//         }
-
-//         // get user by connction id 
-//         public string GetUserIdByConnectionID(string connectionId)
-//         {
-//             if (  connectionId != "undefined" &&  connectionId != "" &&  connectionId is not null )
-//             {
-//                 lock(Users)
-//                 {
-//                     var list = Users.Where(x => x.Value == connectionId).Select(x => x.Key).FirstOrDefault();
-//                     if (list is not null)
-//                     {
-//                         return list;
-//                     } else
-//                     {
-//                         return "";
-//                     }
-//                 }
-//             } else
-//             {
-//                 return "";
-//             }
-//         }
-
-//         public string GetConnectionIdByUser(string user) // user represent userid
-//         {
-//             if(user != "undefined" && user != "")
-//             {
-//                 lock (Users)
-//                 {
-//                     var list = Users.Where(x => x.Key == user).Select(x => x.Value).FirstOrDefault();
-//                     if (list is not null)
-//                     {
-//                         return list;
-//                     } else
-//                     {
-//                         return "";
-//                     }
-//                 }
-//             } else
-//             {
-//                 return "";
-//             }
-//         }
-
-
-
-//     }
-// }
--- backend/realTimeChat/Services/RealTimeChatService.cs
+++ backend/realTimeChat/Services/RealTimeChatService.cs
@@ -1,129 +1,70 @@
 using realTimeServices.Protos;
 using Grpc.Net.Client;
 
-namespace realTimeServices.Services
+namespace realTimeChat.Services
 {
-    public class RealTimeChatClient
+    
+public class RealTimeChatClient
+{
+    private readonly RealTimeChatService.RealTimeChatServiceClient _client;
+
+    public RealTimeChatClient(IConfiguration config)
     {
-        private readonly RealTimeChatService.RealTimeChatServiceClient _client;
+        var apiUrl = config["GrpcServices:ApiService"] ?? "http://api-service:81";
+            var channel = GrpcChannel.ForAddress(apiUrl);
+        // var channel = GrpcChannel.ForAddress("http://localhost:5001");
+        _client = new RealTimeChatService.RealTimeChatServiceClient(channel);
+    }
 
-        // Dependency Injection pulls the IConfiguration to securely read K8s variables
-        public RealTimeChatClient(IConfiguration config)
+
+    public void SendMessage(string content, string sender, string recever)
+    {
+        var request = new MessageRequest
         {
-            var apiServiceUrl = config["GrpcServices:ApiService"] ?? "http://localhost:5001";
-            var channel = GrpcChannel.ForAddress(apiServiceUrl);
-            _client = new RealTimeChatService.RealTimeChatServiceClient(channel);
+            Content = content,
+            Sender = sender,
+            Recever = recever
+        };
+
+        try
+        {
+            var response = _client.SendMessage(request);
+            Console.WriteLine($"Received message: {response.Message}");
         }
-
-        public void SendMessage(string content, string sender, string recever)
+        catch (Exception ex)
         {
-            var request = new MessageRequest
-            {
-                Content = content,
-                Sender = sender,
-                Recever = recever
-            };
+         Console.WriteLine($"Warining : could not send message to amin api via grpc : {ex.Message}");
+            
+        }
+    }
 
+
+    // get user frinds using grpc form api
+    public List<string> GetUsersIdes(string id)
+    {
+        var request = new UserID
+        {
+            Userid = id
+        };
+        if (request.Userid != "undefind" && request.Userid != "")
+        {
             try
             {
-                var response = _client.SendMessage(request);
-                Console.WriteLine($"Received message: {response.Message}");
+                var response = _client.GetUserFollowingFollowers(request);
+                var userIDsList = response.UserIDsLists.FirstOrDefault();
+                if (userIDsList != null )
+                {
+                    return userIDsList.UserIdsList.ToList();
+                }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Warining : could not send message to amin api via grpc : {ex.Message}");
+                 Console.WriteLine($"Warining : could not conntxt to amin api for user reslatjionships : {ex.Message}");
             }
         }
-
-        public List<string> GetUsersIdes(string id)
-        {
-            var request = new UserID { Userid = id };
-
-            if (request.Userid != "undefind" && request.Userid != "")
-            {
-                try
-                {
-                    var response = _client.GetUserFollowingFollowers(request);
-                    var userIDsList = response.UserIDsLists.FirstOrDefault();
-                    if (userIDsList != null)
-                    {
-                        return userIDsList.UserIdsList.ToList();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Warining : could not conntxt to amin api for user reslatjionships : {ex.Message}");
-                }
-            }
-            return new List<string>();
-        }
+        return new List<string>();
     }
+
+
 }
-
-// using realTimeServices.Protos;
-// using Grpc.Net.Client;
-
-
-// public class RealTimeChatClient
-// {
-//     private readonly RealTimeChatService.RealTimeChatServiceClient _client;
-
-//     public RealTimeChatClient()
-//     {
-//         var apiUrl = Environment.GetEnvironmentVariable("GrpcServices__ApiService") ?? "http://api-service:81";
-//             var channel = GrpcChannel.ForAddress(apiUrl);
-//         // var channel = GrpcChannel.ForAddress("http://localhost:5001");
-//         _client = new RealTimeChatService.RealTimeChatServiceClient(channel);
-//     }
-
-
-//     public void SendMessage(string content, string sender, string recever)
-//     {
-//         var request = new MessageRequest
-//         {
-//             Content = content,
-//             Sender = sender,
-//             Recever = recever
-//         };
-
-//         try
-//         {
-//             var response = _client.SendMessage(request);
-//             Console.WriteLine($"Received message: {response.Message}");
-//         }
-//         catch (Exception ex)
-//         {
-//          Console.WriteLine($"Warining : could not send message to amin api via grpc : {ex.Message}");
-            
-//         }
-//     }
-
-
-//     // get user frinds using grpc form api
-//     public List<string> GetUsersIdes(string id)
-//     {
-//         var request = new UserID
-//         {
-//             Userid = id
-//         };
-//         if (request.Userid != "undefind" && request.Userid != "")
-//         {
-//             try
-//             {
-//                 var response = _client.GetUserFollowingFollowers(request);
-//                 var userIDsList = response.UserIDsLists.FirstOrDefault();
-//                 if (userIDsList != null )
-//                 {
-//                     return userIDsList.UserIdsList.ToList();
-//                 }
-//             }
-//             catch (Exception ex)
-//             {
-//                  Console.WriteLine($"Warining : could not conntxt to amin api for user reslatjionships : {ex.Message}");
-//             }
-//         }
-//         return new List<string>();
-//     }
-
-
-// }
+}
